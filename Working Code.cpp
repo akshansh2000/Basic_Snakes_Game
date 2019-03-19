1ba65bebf03294976a0de410330bdd3b3b473c7b
Akshansh Bhanjana <akshansh2000@gmail.com>
2019-03-19 12:37:16 +0530

    Added the Setup() and Logic() functions, code finally works

--- Working Code.cpp
+++ Working Code.cpp
@@ -13,6 +13,17 @@ enum Direction { STOP,
                  DOWN };
 Direction dir;
 
+void Setup() {
+    gameover = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitx = rand() % width;
+    fruity = rand() % height;
+    score = 0;
+    ntail = 0;
+}
+
 void Draw() {
     system("cls");
     for (int i = 0; i < width; ++i)
@@ -74,6 +85,50 @@ void Input() {
             gameover = true;
             break;
         }
+    }
+}
+
+void Logic() {
+    int prevx = tailx[0], prevy = taily[0], prev2x, prev2y;
+    tailx[0] = x, taily[0] = y;
+    for (int i = 1; i < ntail; ++i) {
+        prev2x = tailx[i];
+        prev2y = taily[i];
+        tailx[i] = prevx;
+        taily[i] = prevy;
+        prevx = prev2x;
+        prevy = prev2y;
+    }
+    switch (dir) {
+    case RIGHT:
+        ++x;
+        break;
+    case LEFT:
+        --x;
+        break;
+    case UP:
+        --y;
+        break;
+    case DOWN:
+        ++y;
+        break;
+    }
+    if (x == width)
+        x = -1;
+    else if (x == -1)
+        x = width;
+    else if (y == height + 1)
+        y = -1;
+    else if (y == -1)
+        y = height + 1;
+    for (int i = 0; i < ntail; i++)
+        if (tailx[i] == x && taily[i] == y)
+            gameover = true;
+    if (x == fruitx && y == fruity) {
+        fruitx = rand() % width;
+        fruity = rand() % height;
+        ++score;
+        ++ntail;
     }
 }
 
